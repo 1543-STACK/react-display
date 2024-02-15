559f7ec6d2bed8cbee1ffb2ab758a9c5e87ae0fe
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-15 12:58:37 +0200

    added files weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather</h1>
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/1543-STACK/react-display"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Anu Govender{" "}
+          </a>{" "}
+          and is open-sourced
+          <a
+            href="https://gthub.com/1543-STACK/rect-display/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            on GitHub
+          </a>{" "}
+          and
+          <a
+            href="https://shimmering-selkie-6de184.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
