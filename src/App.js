7f4afb3b9b1a7832d98384c820b296b234732577
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-15 17:34:09 +0200

    added new files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Weather</h1>
-        <footer>
-          This project was coded by
-          <a
-            href="https://github.com/1543-STACK/react-display"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Anu Govender{" "}
-          </a>{" "}
-          and is open-sourced
-          <a
-            href="https://gthub.com/1543-STACK/rect-display/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            on GitHub
-          </a>{" "}
-          and
-          <a
-            href="https://shimmering-selkie-6de184.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            hosted on Netlify
-          </a>
-        </footer>
+    <body>
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Durban" />
+          <footer>
+            This project was coded by
+            <a
+              href="https://github.com/1543-STACK/react-display"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Anu Govender{" "}
+            </a>{" "}
+            and is open-sourced
+            <a
+              href="https://gthub.com/1543-STACK/rect-display/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              on GitHub
+            </a>{" "}
+            and
+            <a
+              href="https://shimmering-selkie-6de184.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              hosted on Netlify
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
